1064b964a4aaaa8ef868bed7bb59c1f7f9b3c345
vikash kumar <vkvikashkumar987@gmail.com>
2024-04-04 19:37:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const https = require('https');
+function fetchLeetCodeData(username, callback) {
+     const query = `
+    {
+         recentSubmissionList(username: "${username}") {
+              title
+              titleSlug
+              timestamp
+              statusDisplay
+              lang
+          }
+          matchedUser(username: "${username}") {
+            username
+            submitStats: submitStatsGlobal {
+              acSubmissionNum {
+                difficulty
+                count
+                submissions
+              }
+            }
+          }
+    }
+    `;
+
+     const url = 'https://leetcode.com/graphql';
+
+     const options = {
+          hostname: 'leetcode.com',
+          port: 443,
+          path: '/graphql',
+          method: 'POST',
+          headers: {
+               'Content-Type': 'application/json',
+               'Content-Length': Buffer.byteLength(JSON.stringify({ query }))
+          }
+     };
+
+     const req = https.request(options, (res) => {
+          let data = '';
+          res.on('data', (chunk) => {
+               data += chunk;
+          });
+          res.on('end', () => {
+               callback(null, data);
+          });
+     });
+
+     req.on('error', (error) => {
+          callback(error, null);
+     });
+
+     req.write(JSON.stringify({ query }));
+     req.end();
+}
+
+module.exports = fetchLeetCodeData;
